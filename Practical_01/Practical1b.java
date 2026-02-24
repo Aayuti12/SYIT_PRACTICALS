e1d2635c826557980a796a0863cea0a428576720
Aayuti Humane <aayutihumane@gmail.com>
2026-02-24 21:02:10 +0530

    Create Practical1b.java

---
+++ Practical_01/Practical1b.java
@@ -0,0 +1,19 @@
+package practical1;
+
+
+public class Practical1b {
+
+public static void main(String[] args) {
+
+for(int i=1; i<=10 ; i++)
+
+{
+
+System.out.println("Reminder " +i+ " to complete your assignment.");
+
+}
+
+}
+
+
+}
